ba5930365a52c5f44de9ed6e57b866dde795d8ff
CANELA LEDEZMA <49193970@ort>
2026-03-13 12:13:31 -0300

    primer commit

---
+++ index.js
@@ -0,0 +1,8 @@
+import matematica from './matematica.js';
+
+let palabra1 = 'auto';
+let palabra2 = 'rueda';
+console.log(palabra1 + " "+ palabra2);
+
+console.log(`hola ${palabra1}, ${palabra2}`);
+

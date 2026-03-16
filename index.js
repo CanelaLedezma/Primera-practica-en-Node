80de43cda24749bb64107e214e8988f321114d06
FELIPE MANUEL ANDRACA <48803376@ort>
2026-03-16 09:01:16 -0300

    ej 05 terminado

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-import matematica from './matematica.js';
+
+import { URL } from 'url';
 
 let palabra1 = 'auto';
 let palabra2 = 'rueda';
@@ -6,3 +7,21 @@ console.log(palabra1 + " "+ palabra2);
 
 console.log(`hola ${palabra1}, ${palabra2}`);
 
+const parsearUrl = (url) =>
+{
+    url = new URL(url)
+const urlParsed =
+{
+    "host": 'https://'+url.hostname,
+    "pathname": url.pathname,
+    "parametros": Object.fromEntries(url.searchParams)
+};
+
+return(urlParsed);
+}
+
+
+
+const url2 = "http://www.ort.edu.ar:8080/alumnos/index.htm?curso=2022&mes=mayo";
+let parsear = parsearUrl(url2);
+console.log(parsear)
